ea800c9610f31d2baca8aa0bb8389faa5bbd4eba
AmitSna <18194916+AmitSna@users.noreply.github.com>
2020-11-11 20:25:49 -0300

    Añadir versión def

--- ejercicios/ej1-pruebas/amitsna.py
+++ ejercicios/ej1-pruebas/amitsna.py
@@ -1,3 +1,14 @@
+"""
+#Versión Función:
+
+def palindromo(sentencia):
+  print(sentencia.lower().replace(" ", "") == sentencia.lower().replace(" ", "")[::-1])
+
+sentencia = input("Ingrese la frase a validar: ")
+
+palindromo(sentencia)
+"""
+
 palindromo = lambda sentencia: print(sentencia.lower().replace(" ", "") == sentencia.lower().replace(" ", "")[::-1])
 
 sentencia = input("Ingrese la frase a validar: ")
