5b5b26ec4d7b1d319da00f8b64018d193a94fc8a
Gabriel La Torre <gala@fi.uba.ar>
2021-04-08 19:34:19 -0300

    Simplificación (#7)
    Está muy bien tu resolución, resuelve el problema, pero es demasiado es esfuerzo hacerlo. El concepto es saber si la letra es mayúscula o no, así que hay que tratar, entonces, de que nuestro código entienda si esa letra es mayúscula o no así no tenemos que "enseñarle" todas las mayúsculas que existen nosotros.

--- ejercicios/CodeWars/Gabriel-Fierro/break_camelCase.py
+++ ejercicios/CodeWars/Gabriel-Fierro/break_camelCase.py
@@ -8,11 +8,10 @@ https://www.codewars.com/kata/5208f99aee097e6552000148/train/python
 
 
 def solution(s):
-    letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     string = ""
 
     for x in s:
-        if(x in letters):
+        if x == x.upper():
             string = string + " "
         string = string + x
 
