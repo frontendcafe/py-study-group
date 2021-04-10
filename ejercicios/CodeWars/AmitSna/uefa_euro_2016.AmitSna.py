75c6f2339db7e9c411e20df36100f83a69ce9413
AmitSna <18194916+AmitSna@users.noreply.github.com>
2021-04-10 01:10:22 -0300

    Update uefa_euro_2016.AmitSna.py

--- ejercicios/CodeWars/AmitSna/uefa_euro_2016.AmitSna.py
+++ ejercicios/CodeWars/AmitSna/uefa_euro_2016.AmitSna.py
@@ -5,7 +5,7 @@ def uefa_euro_2016(teams, scores):
     return f"At match {teams[0]} - {teams[1]}, teams played draw."
   else:
     highest_score = max(scores)
-    winner_team = scores.index(highest_score)
-    winner = teams[winner_team]
-    return f"At match {teams[0]} - {teams[1]}, {winner} won!"
+    winner_index = scores.index(highest_score)
+    winner_team = teams[winner_index]
+    return f"At match {teams[0]} - {teams[1]}, {winner_team} won!"
   
