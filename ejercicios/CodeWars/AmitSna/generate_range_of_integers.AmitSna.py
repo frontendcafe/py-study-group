ec8f17e4fd29521bdd4b123f960d61ef093635e3
AmitSna <18194916+AmitSna@users.noreply.github.com>
2021-04-10 01:30:20 -0300

    Update generate_range_of_integers.AmitSna.py

--- ejercicios/CodeWars/AmitSna/generate_range_of_integers.AmitSna.py
+++ ejercicios/CodeWars/AmitSna/generate_range_of_integers.AmitSna.py
@@ -1,1 +1,9 @@
 generate_range = lambda min, max, step: list(range(min, max, step))
+
+def generate_range(min, max, step):
+    result_list = []
+    while min < max:
+        result_list.append(min)
+        min += step
+    return result_list
+  
