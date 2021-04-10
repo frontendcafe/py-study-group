9724477006b694ce91ed8e722e37821f70b4828a
AmitSna <18194916+AmitSna@users.noreply.github.com>
2021-04-10 01:02:47 -0300

    Update math_operations.AmitSna.py

--- ejercicios/CodeWars/AmitSna/math_operations.AmitSna.py
+++ ejercicios/CodeWars/AmitSna/math_operations.AmitSna.py
@@ -1,1 +1,4 @@
-ice_brick_volume = lambda radius, bottle_length, rim_length: ((radius*2)**2 / 2) * (bottle_length - rim_length)
+ice_brick_volume = lambda radius, bottle_length, rim_length: ((radius * 2) ** 2 / 2) * (bottle_length - rim_length)
+
+def ice_brick_volume(radius, bottle_length, rim_length):
+  return ((radius * 2) ** 2 / 2) * (bottle_length - rim_length)
