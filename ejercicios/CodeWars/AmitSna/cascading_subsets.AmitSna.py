7c586ce30d25f7e81b311d6b48688453a61c89b2
AmitSna <18194916+AmitSna@users.noreply.github.com>
2021-04-14 22:58:20 -0300

    Update cascading_subsets.AmitSna.py

--- ejercicios/CodeWars/AmitSna/cascading_subsets.AmitSna.py
+++ ejercicios/CodeWars/AmitSna/cascading_subsets.AmitSna.py
@@ -1,3 +1,5 @@
+each_cons = lambda lst, n: [lst[section: section + n] for section in range(0, len(lst) - n + 1)]
+
 def each_cons(lst, n):
     cascaded = []
     for section in range(0, len(lst) - n + 1):
