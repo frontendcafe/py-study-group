d2a9a03b6bf73c91e81ac956b759ae7a6a16a46e
AmitSna <18194916+AmitSna@users.noreply.github.com>
2021-04-14 22:17:18 -0300

    Create give_me_a_diamond.AmitSna.py

---
+++ ejercicios/CodeWars/AmitSna/give_me_a_diamond.AmitSna.py
@@ -0,0 +1,10 @@
+def diamond(n):
+    if n < 1 or n%2==0:
+        return None
+    diamond = ""
+    for amount in range(1, n + 1, 2):
+        diamond += " " * ((n - amount) // 2) + "*" * amount + "\n"
+    for amount in range(n - 2, 0, -2):
+        diamond += " " * ((n - amount) // 2) + "*" * amount + "\n"
+    print(diamond)
+    return diamond
