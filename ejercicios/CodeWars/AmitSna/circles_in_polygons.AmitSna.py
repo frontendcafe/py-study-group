063da62cd0d674723717ade7e54c495bef8604c9
AmitSna <18194916+AmitSna@users.noreply.github.com>
2021-07-11 16:49:45 -0300

    Create circles_in_polygons.AmitSna.py

---
+++ ejercicios/CodeWars/AmitSna/circles_in_polygons.AmitSna.py
@@ -0,0 +1,6 @@
+from math import tan, pi
+circle_diameter = lambda sides, side_length: side_length / tan(pi / sides)
+
+from math import tan, pi
+def circle_diameter(sides, side_length):
+  return side_length / tan(pi / sides)
